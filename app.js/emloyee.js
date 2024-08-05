f27a2e490dadebf626c4fd68d0d88358d50e7406
Jerry Bossan <Jerry@Bodams-MacBook-Pro.local>
2024-08-06 00:01:32 +0100

    just finished commiting

---
+++ app.js/emloyee.js
@@ -0,0 +1,28 @@
+
+const employee1 = {
+	name: 'bossan jeremiah',
+	workExpereriienceYears: 3years
+	salary: $789.98
+	yearOfEmployment: 2024
+	grade: 18
+	maritalStatus: married
+	retirementDate: this.yearOfEmloyment + 30
+}
+const employee2 = {
+	name: 'kingsley jacob',
+	workExpereriienceYears: 2years
+	salary: $759.98
+	yearOfEmployment: 2023
+	grade: 16
+	maritalStatus: married
+	retirementDate: this.yearOfEmloyment + 30
+}
+const employee3 = {
+	name: 'steven wanbebe',
+	workExpereriienceYears: 1years
+	salary: $729.98
+	yearOfEmployment: 2022
+	grade: 15
+	maritalStatus: married
+	retirementDate: this.yearOfEmloyment + 30
+}
